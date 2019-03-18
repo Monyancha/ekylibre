ef0ff891d8aed8e39b8f18fe71a2d1fc21ab8fb5
Rémi Piotaix <remi@piotaix.fr>
2019-03-18 16:55:42 +0100

    Refactor fixed_asset_depreciation period computation with tests

---
+++ app/services/depreciation_calculator.rb
@@ -0,0 +1,59 @@
+class DepreciationCalculator
+
+  def initialize(financial_year_reference, depreciation_period)
+    @fy_reference = financial_year_reference
+    @depreciation_period = depreciation_period
+  end
+
+  # We base the default start day of the financial year on the day following the reference FinancialYear as
+  # it can have an length that is not 1 year
+  def financial_year_start_day
+    @fy_reference.stopped_on + 1.day
+  end
+
+  def depreciation_period(started_on, yearly_percentage)
+    months = monthly_periods(started_on, yearly_percentage)
+
+    return months if @depreciation_period == :monthly
+
+    fy_start = financial_year_start_day
+    fy_shift = (fy_start - fy_start.beginning_of_year).days
+
+    groups = case @depreciation_period
+             when :quarterly
+               months.group_by do |m|
+                 shifted = m.started_on - fy_shift
+                 [shifted.year, shifted.month % 3]
+               end
+             when :yearly
+               months.group_by { |m_started_on, _, _| (m_started_on - fy_shift).year }
+             else
+               return nil
+             end
+
+    groups.map { |_, v| ([v.first.first, v.last.second, v.map(&:third).reduce(&:+)]) }
+  end
+
+  def monthly_periods(started_on, yearly_percentage)
+    return if(yearly_percentage == 0)
+
+    depreciation_duration = 100 / yearly_percentage * 360 # in days
+
+    months = []
+    current = started_on
+    remaining_days = depreciation_duration
+    while remaining_days >= 30
+      month_end = current.end_of_month
+      month_duration = 30 - current.day + 1
+      months << [current, month_end, month_duration]
+
+      current = month_end + 1.day
+      remaining_days -= month_duration
+    end
+    if remaining_days > 0
+      months << [current, current + remaining_days.days, remaining_days]
+    end
+
+    months
+  end
+end
